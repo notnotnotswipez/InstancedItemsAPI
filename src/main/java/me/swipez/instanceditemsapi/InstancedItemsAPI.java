cc88478e6216d5d68f5f0b200047cb91feea69e9
notnotnotswipez <strubbe06@gmail.com>
2021-11-28 19:21:29 -0500

    Use logger and finalize release 1.3.

--- src/main/java/me/swipez/instanceditemsapi/InstancedItemsAPI.java
+++ src/main/java/me/swipez/instanceditemsapi/InstancedItemsAPI.java
@@ -3,13 +3,15 @@ package me.swipez.instanceditemsapi;
 import org.bukkit.ChatColor;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.util.logging.Level;
+
 public final class InstancedItemsAPI {
 
     private static JavaPlugin plugin;
 
     public static void setPlugin(JavaPlugin javaPlugin){
         plugin = javaPlugin;
-        System.out.println(ChatColor.GREEN+"Instanced Items API "+ChatColor.BLUE+"initialized for "+plugin.getName());
+        plugin.getLogger().log(Level.INFO,ChatColor.GREEN+"[Instanced Items API] "+ChatColor.BLUE+"Initialized for "+plugin.getName());
     }
 
     public static JavaPlugin getPlugin() {
